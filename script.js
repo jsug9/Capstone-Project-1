8811329b36aff355add6010391369c4eccf5a02e
Augusto <jesus_augga_99@icloud.com>
2022-03-04 22:39:08 -0500

    add mobile menu

---
+++ script.js
@@ -0,0 +1,31 @@
+// Mobile Menu
+
+function openNav() {
+  document.getElementById('menu').style.width = '100%';
+}
+
+function closeNav() {
+  document.getElementById('menu').style.width = '0%';
+}
+
+document.getElementById('openbtn').addEventListener('click', () => {
+  openNav();
+});
+
+document.getElementById('closebtn').addEventListener('click', () => {
+  closeNav();
+});
+
+document.querySelectorAll('.nav-bar-link').forEach((n) => n.addEventListener('click', () => {
+  document.getElementById('menu').style.width = '0%';
+}));
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 800) {
+    document.getElementById('menu').style.width = 'fit-content';
+  }
+
+  if (window.innerWidth <= 800) {
+    document.getElementById('menu').style.width = '0%';
+  }
+});
