17e5bc073e58d2095d9351f6e9bdcf1b67ff505f
Augusto <jesus_augga_99@icloud.com>
2022-03-07 11:04:49 -0500

    update content of sections to match the band information

--- script.js
+++ script.js
@@ -34,40 +34,40 @@ window.addEventListener('resize', () => {
 // Band Members
 const bandMembers = [
   {
+    image: '/Resources/band/member0.jpeg',
+    name: 'Alejandro Chavez',
+    position: 'Lead Singer',
+    description: 'Alejo used to be a solo artist who composed covers for his Instagram account, the band inmediately noticed his talent and potential, and that\'s how he joined the band.',
+  },
+  {
     image: '/Resources/band/member1.jpeg',
-    name: 'Alejandro Chavez',
-    position: 'Lead Singer',
+    name: 'Eduardo Palacios',
+    position: 'Drummer',
+    description: 'Eduardo\'s favorite instrument has always been the drums, he learned it from a young age and now he is one of the founders of the band',
+  },
+  {
+    image: '/Resources/band/member2.jpeg',
+    name: 'Sebastian Valencia',
+    position: 'Lead Guitarrist',
     description: 'Specialist in covers and stuff like that',
   },
   {
-    image: '/Resources/band/member1',
+    image: '/Resources/band/member3.jpeg',
     name: 'Alejandro Chavez',
-    position: 'Lead Singer',
+    position: 'Rhythm Guitarrist',
     description: 'Specialist in covers and stuff like that',
   },
   {
-    image: '/Resources/band/member1',
+    image: '/Resources/band/member4.jpeg',
     name: 'Alejandro Chavez',
-    position: 'Lead Singer',
+    position: 'Bass',
     description: 'Specialist in covers and stuff like that',
   },
   {
-    image: '/Resources/band/member1',
+    image: '/Resources/band/member5.jpeg',
     name: 'Alejandro Chavez',
-    position: 'Lead Singer',
-    description: 'Specialist in covers and stuff like that',
-  },
-  {
-    image: '/Resources/band/member1',
-    name: 'Alejandro Chavez',
-    position: 'Lead Singer',
-    description: 'Specialist in covers and stuff like that',
-  },
-  {
-    image: '/Resources/band/member1',
-    name: 'Alejandro Chavez',
-    position: 'Lead Singer',
-    description: 'Specialist in covers and stuff like that',
+    position: 'Music Producer',
+    description: 'He may doesn\'t play any particular instrument but still has an important role on the band mixing the instruments and making sure everything is in perfect armony',
   },
 ];
 
